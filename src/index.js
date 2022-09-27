8a1d5841056d6bc3fd09a2d0895a62842e1e1f9f
Bastien-Gaillard <gaillardbastienjf@gmail.com>
2022-09-27 11:10:30 +0200

    Création du projet

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(token);
