920cf6213dc73637af1dcdaa6335441d22cb19bb
Bastien-Gaillard <gaillardbastienjf@gmail.com>
2022-09-27 15:39:02 +0200

    1er commande Discord

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const axios = require('axios');
+const fs = require("fs"); 
+const config = JSON.parse(fs.readFileSync("./config.json"));
+const token = config.token;
+const meteoKey = config.meteoKey;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-// Login to Discord with your client's token
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	const { commandName } = interaction;    
+    if (commandName === 'user') {
+        weather();
+		await interaction.reply("sfzf");
+	}
+});
+
 client.login(token);
+const weather = function () { 
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=22.7196&lon=75.8577&appid=${meteoKey} `)
+    .then((response) => {
+        return response;
+    }).catch((e) => {
+        console.log("searchChannels ERROR:", e);
+    });
+} 
