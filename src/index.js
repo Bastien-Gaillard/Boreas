0e66b42d6169ac60bdcb160155fb4dce9a0502de
Bastien-Gaillard <gaillardbastienjf@gmail.com>
2022-10-03 17:00:26 +0200

    Correction de bug

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
 const axios = require('axios');
-const fs = require("fs"); 
+const fs = require("fs");
 const { channels } = require('discord.js');
 const config = JSON.parse(fs.readFileSync("./config.json"));
 const token = config.token;
@@ -10,44 +10,44 @@ const guildId = config.guildId;
 const clientId = config.clientId;
 const meteoKey = config.meteoKey;
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const rest = new REST(({ version: '10'})).setToken(token);
+const rest = new REST(({ version: '10' })).setToken(token);
 const Data = require("./data");
 
-//Interaction client 
-client.on( 'interactionCreate', async interaction => {
+//Interaction client
+client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
     var city;
     var data;
     //Si la commande tapé est 'weather' on appele la fonction weather() et on l'affiche dans un embed
     if (interaction.commandName === 'weather') {
+		await interaction.deferReply();
         city = interaction.options.getString('city');
-        console.log(city);
-        if(city != null){
+        console.time("timer");
+        if (city != null) {
             try {
-                data =  await weather(city);
-                if(data == null) return await interaction.reply("An error has occurred"); 
-                display(interaction, data);
-            } catch (error) {
-                console.log(error);
-            }  
+                data = await weather(city);
+                if (typeof data == "string" || data == null || typeof data == "undefined") return await interaction.editReply(data || "An error has occurred");
+                await display(interaction, data);
+            } catch (e) {
+                console.log(e);
+            }
         } else {
             try {
-            city = randomCity();
-            data =  await weather(city);
-            if(data == null) return await interaction.reply("An error has occurred"); 
-            display(interaction, data);
-            } catch (error) {
-                console.log(error); 
+                city = await randomCity();
+                data = await weather(city);
+                if (typeof data == "string" || data == null || typeof data == "undefined") return await interaction.editReply(data || "An error has occurred");
+                await display(interaction, data);
+            } catch (e) {
+                console.log(e);
             }
         }
-        
+        console.timeEnd("timer");
     }
 });
 //On récupere la météo de la ville voulu
-const weather = async function (city) { 
+const weather = async function (city) {
     try {
-        let request =  await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${meteoKey} `);
-        console.log("La reponse est : " + request);
+        let request = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${meteoKey} `);
         return new Data(
             request['data']['name'],
             request['data']['weather'][0]['description'],
@@ -60,83 +60,93 @@ const weather = async function (city) {
             request['data']['main']['humidity'],
             request['data']['visibility']
         );
-    } catch(e){
-        console.log("Erreur " + e);
-        return null;
+    } catch (e) {
+        console.error("Erreur", e.response?.data?.message);
+        return e.response?.data?.message;
     }
-} 
-const display = async function (interaction, data) { 
-    
-    var information = "**" + data.city + ", " + data.country + "**" + 
-        "\n" + data.celsiusTemp(data.temp) + " °C" + 
+}
+const display = async function (interaction, data) {
+    var information = "**" + data.city + ", " + data.country + "**" +
+        "\n" + data.celsiusTemp(data.temp) + " °C" +
         "\n     Feels like " + data.celsiusTemp(data.feels_like) + " °C. " + data.description.charAt(0).toUpperCase() + data.description.slice(1);
-    const exampleEmbed = new EmbedBuilder()
-    .setColor(0x0099FF)
-    .setDescription(information)
-    .addFields(
-        { name: data.wind_speed + "m/s " + data.orientation(data.wind_deg), value: "Humidity: " + data.humidity + "%", inline: true},
-        { name: data.pressure + "hPa", value: "Visibility: " + data.visibilityKm(data.visibility) + "km", inline: true },
-    );
-    interaction.reply({ embeds: [exampleEmbed] });
-}
-const getCountry = async function () { 
-    
     try {
-        return await axios.get("https://countriesnow.space/api/v0.1/countries")
-        .then((reponse) => {
-            //return tous les pays avec leurs villes
-            return reponse['data']['data'];
-        }).catch((e) => {
-            console.log("searchChannels ERROR:", e);
-        });
-    } catch(e){
-        console.log("Erreur " + e);
+        const exampleEmbed = new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setDescription(information)
+            .addFields(
+                { name: data.wind_speed + "m/s " + data.orientation(data.wind_deg), value: "Humidity: " + data.humidity + "%", inline: true },
+                { name: data.pressure + "hPa", value: "Visibility: " + data.visibilityKm(data.visibility) + "km", inline: true },
+            );
+        await interaction.editReply({ embeds: [exampleEmbed] });
+    } catch (e) {
+        console.error("L'erreur est", e)
+    }
+
+}
+
+const getCountries = async function () {
+    try {
+        let request = await axios.get("https://countriesnow.space/api/v0.1/countries");
+        // console.log("La reponse est : ", request);
+        return request.data.data;
+    } catch (e) {
+        console.error("Erreur", e);
     }
 }
-const getCity = async function () { 
-    var cities = [];
-    var country = await getCountry();
-    for(let i = 0; i < country.length; i++){
-        cities.push(country[i]['cities'][0]);
-        if(cities[i] == undefined){
-            cities.splice(i, 1);
+const getCities = async function () {
+    var allCitites = [];
+    var countries = await getCountries();
+    countries.forEach(country => {
+        if (country['cities'] != undefined) {
+            country['cities'].forEach(city => {
+                allCitites.push(city);
+            });
         }
+    });
+    return allCitites;
+}
+var last = (Date.now() / 1000 / 60);
+var cities = [];
+const getCache = async function () {
+    if((Date.now() / 1000 / 60) - last >= 0.4){
+        last = (Date.now() / 1000 / 60);
+        console.log("Trop vieux"); 
+        cities = await getCities();
     }
     return cities;
 }
-const randomCity = async function(){
-    var cities = await getCity();
-    return cities[Math.round( Math.random() * cities.length )];
+const randomCity = async function () {
+    let cities = await getCache();
+    return cities[Math.round(Math.random() * cities.length)];
 }
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
 });
 
-(async ()=>{
-
+(async () => {
     const commands = [
         {
             name: 'weather',
             description: 'Give city weather',
             options: [
-              {
-                  name: 'city',
-                  description: 'the city',
-                  type: 3
-              }
+                {
+                    name: 'city',
+                    description: 'the city',
+                    type: 3
+                }
             ]
         }
     ];
     try {
-      console.log('Started refreshing application (/) commands.');
-      await rest.put(Routes.applicationCommands(clientId), {
-        body: commands,
-      });
-      console.log('Successfully reloaded application (/) commands.');
+        cities = await getCities();
+        console.log('Started refreshing application (/) commands.');
+        await rest.put(Routes.applicationCommands(clientId), {
+            body: commands,
+        });
+        console.log('Successfully reloaded application (/) commands.');
     } catch (err) {
-      console.log(err);
+        console.log(err);
     }
 })()
 
 client.login(token);
-
